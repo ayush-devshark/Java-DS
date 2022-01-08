e80e78c2f997f64a5cb366dd8a5025f56c6be3a6
ayush-devshark <ayushagarwal.dev@gmail.com>
2022-01-08 17:12:08 +0530

    optimise bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,15 +3,19 @@ public class BubbleSort {
     int [] arr = {2,5,7,8,1,3};
 
     int n = arr.length;
-
+    boolean flag;
     for( int i = 0;i<n-1;i++){
+      flag = false;
         for( int j = 0; j< n-1-i;j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = true;
             }
         }
+        // If no swaps break;
+        if(!flag) break;
       }
 
       for(int x: arr){
